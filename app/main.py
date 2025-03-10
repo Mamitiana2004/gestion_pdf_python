193c2eee9452412ec95a0b16f0c547263f7e55fa
Faneva Mamitiana <mamitianafaneva2004@gmail.com>
2025-03-10 17:14:28 +0100

    update

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes import usersRoute,pdfRoute,adminRoute,shipperRoute
+from app.routes import usersRoute,pdfRoute,adminRoute,shipperRoute,consigneRoute
 from app.middleware.auth_middleware import JWTAuthMiddleware
 
 
@@ -28,4 +28,5 @@ app.add_middleware(JWTAuthMiddleware)
 app.include_router(pdfRoute.router, prefix="/api/pdf",tags=["PDFs"])
 app.include_router(usersRoute.router, prefix="/users", tags=["Users"])
 app.include_router(shipperRoute.router,prefix="/api/shipper",tags=["shippers"])
+app.include_router(consigneRoute.router,prefix="/api/consigne",tags=["consignees"])
 app.include_router(adminRoute.router,prefix="/api/admin",tags=["ADMIN"])
--- app/routes/consigneRoute.py
+++ app/routes/consigneRoute.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from app.services.consigneeService import getAllConsigne
+from app.services.consigneeService import getAllConsigne,createNewConsigne,updateConsigne,deleteConsigne
+from pydantic import BaseModel
 
 router = APIRouter()
 
@@ -7,3 +8,28 @@ router = APIRouter()
 def getAll():
     result = getAllConsigne()
     return result
+
+
+class AddConsigne(BaseModel):
+    name:str
+    adresse:str
+
+@router.post("create")
+def create_new_consigne(request:AddConsigne):
+    createNewConsigne(name=request.name,adresse=request.adresse)
+    return {"success":"TRUE"}
+
+class AllConsigne(BaseModel):
+    id:int
+    name:str
+    adresse:str
+
+@router.put("/update")
+def update_consigne(request:AllConsigne):
+    updateConsigne(id=request.id,name=request.name,adresse=request.adresse)
+    return {"success":"TRUE"}
+
+@router.delete("/delete/{id}")
+def delete_consigne(id:int):
+    deleteConsigne(id)
+    return {"success":"TRUE"}
